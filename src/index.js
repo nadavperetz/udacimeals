bbc25c7ee2ca2510ec09914053d0a30c1ce46c5b
nadavperetz <nadavperetz@gmail.com>
2017-10-04 13:15:33 -0300

    pushing to github

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import './index.css';
+import App from './components/App';
+import {createStore} from 'redux';
+import reducer from './reducers'
+
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(<Provider store={store}>
+                    <App />
+                </Provider>,
+                document.getElementById('root'));
+
